5b3f1753a1ed0ed3b97715af3ad8dd2add3a8fd9
jkluio132465 <jkluio132465@gmail.com>
2017-11-22 14:40:52 +0800

    change(),after()

--- database/migrations/2017_11_22_062358_add_is_feature_in_posts_table.php
+++ database/migrations/2017_11_22_062358_add_is_feature_in_posts_table.php
@@ -12,21 +12,26 @@ class AddIsFeatureInPostsTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::table('posts', function (Blueprint $table) {
-            //
-        });
-    }
+      {
+          Schema::table('posts', function (Blueprint $table) {
+            $table->string('title',500) ->change();
+            $table ->boolean('is_feature') ->default(false) ->after('content');
+  
+          });
+     }
+ 
+     /**
+      * Reverse the migrations.
+      *
+      * @return void
+      */
+     public function down()
+     {
+         Schema::table('posts', function (Blueprint $table) {
+             //
+            $table->string('title') ->change();
+            $table ->dropColumn('is_feature');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('posts', function (Blueprint $table) {
-            //
-        });
-    }
-}
+         });
+     }
+ }
