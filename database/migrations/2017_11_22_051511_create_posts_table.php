9227c9bc33f61f229f0db3ec02ddf41dfd5e3f4d
jkluio132465 <jkluio132465@gmail.com>
2017-11-22 14:25:57 +0800

    新增欄位

--- database/migrations/2017_11_22_051511_create_posts_table.php
+++ database/migrations/2017_11_22_051511_create_posts_table.php
@@ -14,11 +14,13 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-
-            $table->increments('id');
             $table->string('title');
             $table->text('content');
+<<<<<<< HEAD:database/migrations/2017_11_22_054158_create_posts_table.php
             $table->text('name');
+=======
+            $table->increments('id');
+>>>>>>> parent of e50297c... 新增兩個資料表:database/migrations/2017_11_22_051511_create_posts_table.php
             $table->timestamps();
         });
     }
@@ -30,6 +32,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
+        $table ->string('title') ->change(); 
+        $table ->dropColumn('is_feature') ;
         Schema::dropIfExists('posts');
     }
 }
